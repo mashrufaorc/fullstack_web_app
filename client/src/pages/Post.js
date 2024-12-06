751da8d32099138daaf3fb4875f2dff7753a054d
Mashrufa Orchi <165407256+mashrufaorc@users.noreply.github.com>
2024-12-05 20:46:42 -0500

    comment section upadated

--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -1,29 +1,69 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import axios from "axios";
 
 function Post() {
-    let { id } = useParams(); // UseParams hook to get the route parameter
-    const [postObject, setPostObject] = useState({});
+  let { id } = useParams();
+  const [postObject, setPostObject] = useState({});
+  const [comments, setComments] = useState([]);
+  const [newComment, setNewComment] = useState("");
 
-    useEffect(() => {
-        axios.get(`http://localhost:3001/posts/byId/${id}`).then((response) => {
-            setPostObject(response.data);
-        });
-    })
-    
-    return (
-        <div className="postPage">
-            <div className="leftSide">
-                <div className="post" id="individual"> 
-                    <div className="title"> {postObject.title}</div>
-                    <div className="postText"> {postObject.postText}</div>
-                    <div className="footer"> {postObject.username}</div>
-                </div>
-            </div>
-            <div className="rightSide"> Comment Section</div>
+  useEffect(() => {
+    axios.get(`http://localhost:3001/posts/byId/${id}`).then((response) => {
+      setPostObject(response.data);
+    });
+
+    axios.get(`http://localhost:3001/comments/${id}`).then((response) => {
+      setComments(response.data);
+    });
+  }, [id]);
+
+  const addComment = () => {
+    axios
+      .post("http://localhost:3001/comments", {
+        commentBody: newComment,
+        PostId: id,
+      })
+      .then((response) => {
+        const commentToAdd = { commentBody: newComment };
+        setComments([...comments, commentToAdd]);
+        setNewComment("");
+      });
+  };
+
+  return (
+    <div className="postPage">
+      <div className="leftSide">
+        <div className="post" id="individual">
+          <div className="title"> {postObject.title} </div>
+          <div className="body">{postObject.postText}</div>
+          <div className="footer">{postObject.username}</div>
         </div>
-    );
+      </div>
+      <div className="rightSide">
+        <div className="addCommentContainer">
+          <input
+            type="text"
+            placeholder="Comment..."
+            value={newComment}
+            onChange={(event) => {
+              setNewComment(event.target.value);
+            }}
+          />
+          <button onClick={addComment}> Add Comment</button>
+        </div>
+        <div className="listOfComments">
+          {comments.map((comment, key) => {
+            return (
+              <div key={key} className="comment">
+                {comment.commentBody}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Post;
