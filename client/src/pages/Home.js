68d191b24b3efa557c5be34e666b4a9c21b3b5c1
Mashrufa Orchi <165407256+mashrufaorc@users.noreply.github.com>
2025-01-09 00:10:57 -0500

    delete posts

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -12,7 +12,7 @@ function Home() {
   let navigate = useNavigate();
 
   useEffect(() => {
-    if (!authState.status) {
+    if (!localStorage.getItem("accessToken")) {
       navigate("/login");
     } else {
       axios
