89f7b3ef143715543e40a19f530910bb8542d5ac
Mashrufa Orchi <165407256+mashrufaorc@users.noreply.github.com>
2025-01-12 01:50:32 -0500

    edits

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Registration from "./pages/Registration";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import Profile from "./pages/Profile";
+import ChangePassword from "./pages/ChangePassword";
 
 import { AuthContext } from "./helpers/AuthContext";
 import "./App.css"; // Import CSS last for proper styling
@@ -74,6 +75,7 @@ function App() {
             <Route path="/registration" element={<Registration />} />
             <Route path="/login" element={<Login />} />
             <Route path="/profile/:id" element={<Profile />} />
+            <Route path="/changepassword" element={<ChangePassword />} />
             <Route path="*" exact component={PageNotFound} />
           </Routes>
         </Router>
